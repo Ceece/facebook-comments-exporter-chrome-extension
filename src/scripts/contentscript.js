c62ac26fc30e2cd9cf572140714484c72cea7dad
Teerapong Manakul <Ceece@users.noreply.github.com>
2022-01-22 11:52:03 +0700

    update download

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,35 +1,63 @@
+/* global chrome */
+const { Blob } = window
 const delay = (ms) => new Promise(resolve => setTimeout(() => resolve(), ms))
-const stripTag = (str = '') => (str + '').replace(/<\/?[^>]+(>|$)/g, '');
+const stripTag = (str = '') => (str + '').replace(/<\/?[^>]+(>|$)/g, '')
 
 function launch () {
   window.open(window.location.href.replace('www.facebook.com', 'm.facebook.com'), '_blank')
 }
 
 async function expand () {
-  for (let index = 0; index < 10; index++) {
+  let count = 0
+  let comments = getCommentElements()
+  while (comments.length !== count) {
     document.querySelectorAll('[id*=next] a').forEach(a => a.click())
     document.querySelectorAll('[id*=prev] a').forEach(a => a.click())
-    // click on subcomments
-    document.querySelectorAll('[data-sigil="replies-see-more"] a').forEach(a => a.click())
-    // click on loadmore subcomments
     document.querySelectorAll('[id*=more] a').forEach(a => a.click())
+    document.querySelectorAll('[data-sigil="replies-see-more"] a').forEach(a => a.click())
     await delay(1000)
+    count = comments.length
+    comments = getCommentElements()
   }
 }
 
-function extract() {
-  const list = []
-
-  const comments = document.querySelectorAll('._2b04') // Find all comments on page
-
-  for (let i = 0; i < comments.length;) {
-    i += findChildComments(comments[i], list) // Comments processing and hierarchy creation
-  }
+function extract () {
+  const list = getComments()
   const header = 'Name, Comment, Profile, Link\n'
   download(
-    header + list.map(row => Object.values(row).slice(0, 4).map(cell => `"${stripTag(cell)}"`).join(',')).join('\n'),
-    `facebook-${new Date().getTime()}.csv`,
-    'text/csv')
+    header + list.map(row => Object.values(row).slice(0, 4).map(cell => `"${stripTag(cell.replace('"', '\\"'))}"`).join(',')).join('\n'),
+    `facebook-${new Date().getTime()}.csv`)
+}
+
+function getCommentElements () {
+  return document.querySelectorAll('[data-sigil="comment-body"]')
+}
+
+function getComments () {
+  const elements = getCommentElements()
+  const comments = []
+  const postedIn = window.location.href
+  for (let i = 0; i < elements.length; i++) {
+    try {
+      const element = elements[i]
+      const name = element.parentElement.children[0].innerText
+      const comment = element.parentElement.children[1].innerText
+      const link = element.parentElement.children[0].querySelector('a').getAttribute('href')
+      comments.push({ name, comment, link: 'https://www.facebook.com' + link, postedIn })
+    } catch (error) {}
+  }
+  return comments
+}
+
+function download (content, fileName) {
+  const a = document.createElement('a')
+  const csv = '\ufeff' + content
+  const file = new Blob([csv], { type: 'text/csv;charset=utf-8;' })
+  a.href = URL.createObjectURL(file)
+  a.download = fileName
+  a.style.display = 'none'
+  a.target = '_blank'
+  a.click()
 }
 
 chrome.extension.onMessage.addListener(async function (message, sender, sendResponse) {
@@ -43,41 +71,3 @@ chrome.extension.onMessage.addListener(async function (message, sender, sendResp
       break
   }
 })
-
-function findChildComments (comment, list) {
-  let index = 0
-  // Process comment
-  const i = comment.querySelector('._2b06')
-
-  let newComment = {}
-
-  if (i !== null) {
-    const name = i.children[0].innerText
-    let link = i.children[0].children[0] && i.children[0].children[0].attributes && (i.children[0].children[0].attributes[i.children[0].children[0].attributes.length - 1].textContent || 'ERRORERROR')
-    link = `https://www.facebook.com${link}`
-    const comment = i.children[1].innerHTML
-    const postedIn = window.location.href
-    newComment = { name, comment, link, postedIn, child: [] }
-    list.push(newComment)
-  }
-
-  const childComments = comment.querySelectorAll('._2b04')
-
-  if (childComments !== 'undefined') {
-    childComments.forEach(childComment => {
-      index += findChildComments(childComment, newComment.child)
-    })
-  }
-
-  return index + 1
-}
-
-function download (content, fileName, contentType) {
-  const a = document.createElement('a')
-  const file = new Blob([content], { type: contentType })
-  a.href = URL.createObjectURL(file)
-  a.download = fileName
-  a.style.display = 'none'
-  a.target = '_blank'
-  a.click()
-}
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,12 +1,8 @@
+/* global chrome */
+
 function expand () {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { expand: true, type: 'expand' })
-  })
-}
-
-function extract () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { extract: true, type: 'extract' })
   })
 }
 
@@ -15,15 +11,8 @@ function launch () {
     chrome.tabs.sendMessage(tabs[0].id, { launch: true, type: 'launch' })
   })
 }
-function exportFile () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { exportFile: true, type: 'exportFile' })
-  })
-}
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('expand').addEventListener('click', expand)
-  document.getElementById('extract').addEventListener('click', extract)
   document.getElementById('launch').addEventListener('click', launch)
-  document.getElementById('export').addEventListener('click', exportFile)
 })
